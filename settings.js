cd362cec2a67115ffcbccb5a480be52c65dd1d3a
Molly-tee <adesanyafeyisolam@gmail.com>
2024-07-02 07:29:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,9 +13,9 @@ global.owner = process.env.OWNER_NUMBER  || '2349025526060' ;  // ADD YOUR NUMBE
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=CYlUATRB#4rdM84KDX_I769-fS28neoRpMttQEwtno1-0Cc8LUyw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
-MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
+MODERATORS : process.env.MODERATORS === undefined ? "2349025526060" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
+OWNER_NAME: process.env.OWNER_NAME || 'feyisola' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
